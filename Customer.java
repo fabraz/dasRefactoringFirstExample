f30e73787dbe6d10bf8dd064fe23b2248d40465e
fabraz <fabricio.braz@gmail.com>
2014-03-19 17:09:25 -0300

    decomposing and redistributing the statement method - extracting frequentRenterPoints method

--- Customer.java
+++ Customer.java
@@ -25,24 +25,14 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			//double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-
 			
-			//thisAmount = each.getCharge();	
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release
-
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += each.getFrequentRenterPoints();
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
 
 			totalAmount += each.getCharge();
-
 		}
 
 		// add footer lines
@@ -53,5 +43,4 @@ class Customer {
 
 		return result;
 	}
-	
 }
