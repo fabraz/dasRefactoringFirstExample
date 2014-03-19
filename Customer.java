c7e6f8314d5d8206d2328ec5c25001ec2cdceef8
fabraz <fabricio.braz@gmail.com>
2014-03-19 18:19:10 -0300

    replacing conditional logic on price code with polymotphism - moving getCharge conditional from Rental.java to Movie.java

--- Customer.java
+++ Customer.java
@@ -26,15 +26,12 @@ class Customer {
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
-
 		}
-
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 
 		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
 				+ " frequent renter points";
-
 		return result;
 	}
 	
--- Movie.java
+++ Movie.java
@@ -1,26 +1,52 @@
-public class Movie {
+class Rental {
+	private Movie _movie;
+	private int _daysRented;
 
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	private String _title;
-	int _priceCode;
-
-	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
 	}
 
-	public int getPriceCode() {
-		return _priceCode;
+	public int getDaysRented() {
+		return _daysRented;
 	}
 
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
+	public Movie getMovie() {
+		return _movie;
 	}
 
-	public String getTitle() {
-		return _title;
+	public double getCharge() {
+		double result = 0;
+		// determine amounts for each line
+		switch (getMovie().getPriceCode()) {
+
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2)
+				result += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+
+			if (getDaysRented() > 3)
+				result += (getDaysRented() - 3) * 1.5;
+			break;
+
+		}
+		return result;
 	}
+
+	public int getFrequentRenterPoints() {
+
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+
+			return 1;
+	}
+
 }
-
