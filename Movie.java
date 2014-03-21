e201fabb8445b699a7c3d286fbe428661e329909
fabraz <fabricio.braz@gmail.com>
2014-03-21 16:10:28 -0300

    replacing conditional logic on price code with polymorphism - change accessors for the price code to use Price.java

--- Movie.java
+++ Movie.java
@@ -5,6 +5,7 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 	private String _title;
 	private int _priceCode;
+	private Price _price;
 	
 	public Movie(String title, int priceCode) {
 		_title = title;
@@ -12,11 +13,24 @@ public class Movie {
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrenPrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 
 	public String getTitle() {
