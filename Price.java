a44bd741b7f613fce67bb6fc99931bc2b7a7ff17
fabraz <fabricio.braz@gmail.com>
2014-03-21 16:37:01 -0300

    replacing conditional logic on price code with polymorphism - getFrequentRenterPoints

--- Price.java
+++ Price.java
@@ -4,12 +4,7 @@ abstract class Price {
 	abstract double getCharge(int daysRented);
 
 	public int getFrequentRenterPoints(int _daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE)
-				&& _daysRented > 1)
-			return 2;
-		else
-
-			return 1;
+		return 1;
 	}
 }
 
@@ -23,12 +18,11 @@ class ChildrenPrice extends Price {
 	double getCharge(int daysRented) {
 		double result = 1.5;
 		// determine amounts for each line
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
 		return result;
 	}
 }
-
 
 class NewReleasePrice extends Price {
 	@Override
@@ -40,9 +34,13 @@ class NewReleasePrice extends Price {
 	double getCharge(int daysRented) {
 		return daysRented * 3;
 	}
-		
-}
 
+	@Override
+	public int getFrequentRenterPoints(int _daysRented) {
+		return (_daysRented > 1) ? 2 : 1;
+
+	}
+}
 
 class RegularPrice extends Price {
 	@Override
@@ -51,11 +49,10 @@ class RegularPrice extends Price {
 	}
 
 	@Override
-	double getCharge(int daysRented) {
+	double getCharge(int _daysRented) {
 		double result = 2;
-				if (daysRented > 2)
-					result += (daysRented - 2) * 1.5;
+		if (_daysRented > 2)
+			result += (_daysRented - 2) * 1.5;
 		return result;
 	}
 }
-
